875bb372ec27a98822902fb1c9b7c8f5e6efff87
kiran <kiran>
2025-05-02 11:32:31 +1000

    Implement GetQuote query handler

--- CurrencyExchange.Application/Quotes/Queries/GetQuote/GetQuoteQueryHandler.cs
+++ CurrencyExchange.Application/Quotes/Queries/GetQuote/GetQuoteQueryHandler.cs
@@ -1,10 +1,20 @@
+using CurrencyExchange.Application.Common.Interfaces;
 using CurrencyExchange.Domain.Quotes;
 using ErrorOr;
 using MediatR;
 
 namespace CurrencyExchange.Application.Quotes.Queries.GetQuote;
 
-public class GetQuoteQueryHandler : IRequestHandler<GetQuoteQuery, ErrorOr<Quote>>
+public class GetQuoteQueryHandler(IQuoteRepository quoteRepository) : IRequestHandler<GetQuoteQuery, ErrorOr<Quote>>
 {
-    public Task<ErrorOr<Quote>> Handle(GetQuoteQuery request, CancellationToken cancellationToken) => new (() => new Quote());
+    public async Task<ErrorOr<Quote>> Handle(GetQuoteQuery request, CancellationToken cancellationToken)
+    {
+        var quote = await quoteRepository.GetByIdAsync(request.Id, cancellationToken);
+        if (quote is null)
+        {
+            return Error.NotFound("Quote", "Quote not found.");
+        }
+
+        return quote;
+    }
 }
